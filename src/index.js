b0f8865fabd8629dc5dad9f7038b47258ef6e43a
Thrapis <50047219+Thrapis@users.noreply.github.com>
2025-08-27 14:36:56 +0300

    feat: add implementation of towel sort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) return [];
+  matrix.forEach((e, i) => {
+    if (i % 2 === 1) {
+      e.reverse();
+    }
+  });
+  return matrix.flat();
 };
